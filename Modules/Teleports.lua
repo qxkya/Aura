12f41da55b179e460cdefbbe320447963e4980a5
qxkya <93773645+qxkya@users.noreply.github.com>
2025-12-23 21:54:22 +0100

    Update Teleports.lua

--- Modules/Teleports.lua
+++ Modules/Teleports.lua
@@ -1,5 +1,6 @@
 return {
     ["Desert"] = Vector3.new(1281, 25, -226),
     ["Spider Boss"] = Vector3.new(-340, -87, 298),
-    ["The Titan"] = Vector3.new(1654, 23, 2312)
+    ["The Titan"] = Vector3.new(1654, 23, 2312),
+    ["The Juggernaut"] = Vector3.new(-1603, 106, -2873)
 }
